5023a61c554dd40f664dbae10aeee503093aa6f2
jypma <jan@ypmania.nl>
2019-03-10 13:53:55 +0100

    MQTT Autodiscover all room sensor attributes

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-version := "0.1.20190308"
+version := "0.1.20190310"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.5.21",
--- src/main/scala/nl/ypmania/sparkmqtt/RoomSensorForwarder.scala
+++ src/main/scala/nl/ypmania/sparkmqtt/RoomSensorForwarder.scala
@@ -1,19 +1,24 @@
 package nl.ypmania.sparkmqtt
 
-import akka.actor.{ Actor, ActorRef }
+import akka.actor.{ Actor, ActorLogging, ActorRef, Timers }
 import akka.util.ByteString
 import nl.ypmania.sparkmqtt.data.Messages.RoomSensor
 import MQTTActor.Topic./
 import MQTTActor.Topic
 import org.json4s.JsonAST._
 import org.json4s.JsonDSL._
+import org.json4s.native.JsonMethods._
+import scala.concurrent.duration._
 
-class RoomSensorForwarder(mqttActor: ActorRef) extends Actor {
+class RoomSensorForwarder(mqttActor: ActorRef) extends Actor with ActorLogging with Timers {
+  import RoomSensorForwarder._
   context.system.eventStream.subscribe(self, classOf[RoomSensor])
+
+  var haveRegistered = Set.empty[Int]
 
   override def receive = {
     case msg: RoomSensor =>
-      val topic = /("rf12") / "roomsensor" / (msg.sender & 0xFF).toString
+      val topic = stateTopic(msg.sender & 0xFF)
 
       def send(t: Topic): (Double => Unit) = v => {
         mqttActor ! MQTTActor.Message(t, ByteString(v.toString), retained = true)
@@ -28,6 +33,58 @@ class RoomSensorForwarder(mqttActor: ActorRef) extends Actor {
       msg.humidity.filter(h => h > 10).map(_ / 10.0).foreach(send(topic / "humidity"))
       msg.lux.foreach(sendI(topic / "lux"))
       msg.motion.foreach(sendI(topic / "motion"))
+
+      register(msg.sender & 0xFF)
+      timers.startSingleTimer(msg.sender, Remove(msg.sender & 0xFF), 10.minutes)
+
+    case Remove(id) =>
+      log.warning("Not heard from {} for a while, removing readings from MQTT.", id)
+      val topic = stateTopic(id)
+      for (name <- Seq("supply", "temperature", "humidity", "lux", "motion")) {
+        mqttActor ! MQTTActor.Message(topic / name, ByteString.empty, retained = true)
+      }
+  }
+
+  def stateTopic(id: Int) = /("rf12") / "roomsensor" / (id & 0xFF).toString
+
+  def registerSensor(id: Int, top: String, dClass: String, unit: String): Unit = {
+    val topic = /("homeassistant") / "sensor" / s"rf12_${id}_${top}" / "config"
+    val config: JObject =
+      ("name" -> s"RF12 ${id} ${dClass}") ~
+      ("state_topic" -> (stateTopic(id) / top).name) ~
+      ("device_class" -> dClass) ~
+      ("unit_of_measurement" -> unit)
+    mqttActor ! MQTTActor.Message(topic, ByteString(pretty(render(config))), retained = true)
+  }
+
+  def registerBinarySensor(id: Int, top: String, dClass: String): Unit = {
+    val topic = /("homeassistant") / "binary_sensor" / s"rf12_${id}_${top}" / "config"
+    val config: JObject =
+      ("name" -> s"RF12 ${id} ${dClass}") ~
+      ("state_topic" -> (stateTopic(id) / top).name) ~
+      ("device_class" -> dClass) ~
+      ("off_delay" -> 5) ~
+      ("payload_on" -> "true") ~
+      ("value_template" -> "{% if (value != '0') %}true{% else %}false{% endif %}")
+
+    mqttActor ! MQTTActor.Message(topic, ByteString(pretty(render(config))), retained = true)
+  }
+
+  def register(id: Int): Unit = {
+    if (!haveRegistered.contains(id)) {
+      log.info("registering {}", id)
+
+      registerSensor(id, "temperature", "temperature", "°C")
+      registerSensor(id, "humidity", "humidity", "%")
+      registerSensor(id, "supply", "battery", "V")
+      registerSensor(id, "lux", "illuminance", "lx")
+      registerBinarySensor(id, "motion", "motion")
+
+      haveRegistered += id
+    }
   }
 }
 
+object RoomSensorForwarder {
+  private case class Remove(id: Int)
+}
